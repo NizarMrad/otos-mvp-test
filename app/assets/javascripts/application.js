a1f27047f2134ecfcb5ab88a36bb78a3d1c54f83
NizarMrad <nizar.mrad.pro@gmail.com>
2020-10-13 19:12:37 +0200

    template settings almost done

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require assets/js/jquery-3.2.1.min.js
+//= require assets/js/nile-slider.js
+//= require assets/js/scrolla.jquery.min.js
+//= require assets/rslider/js/jquery.themepunch.tools.min.js
+//= require assets/rslider/js/jquery.themepunch.revolution.min.js
+//= require assets/rslider/js/extensions/revolution.extension.actions.min.js
+//= require assets/rslider/js/extensions/revolution.extension.carousel.min.js
+//= require assets/rslider/js/extensions/revolution.extension.kenburn.min.js
+//= require assets/rslider/js/extensions/revolution.extension.layeranimation.min.js
+//= require assets/rslider/js/extensions/revolution.extension.migration.min.js
+//= require assets/rslider/js/extensions/revolution.extension.navigation.min.js
+//= require assets/rslider/js/extensions/revolution.extension.parallax.min.js
+//= require assets/rslider/js/extensions/revolution.extension.slideanims.min.js
+//= require assets/rslider/js/extensions/revolution.extension.video.min.js
+//= require assets/js/YouTubePopUp.jquery.js
+//= require assets/js/owl.carousel.min.js
+//= require assets/js/imagesloaded.min.js
+//= require assets/js/popper.min.js
+//= require assets/js/bootstrap.min.js
+//= require assets/js/imagesloaded.min.js
+//= require assets/js/jquery.filterizr.min.js
+//= require assets/js/wow.min.js
+//= require assets/js/custom.js
